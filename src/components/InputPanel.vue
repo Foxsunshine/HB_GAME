0904310fd6a6bf5bd4bc49d8a9d44389e2481526
Foxsunshine <kaihatsu.chien.jan@gmail.com>
2024-06-15 20:11:55 +0900

    Implement the digital input feature.

--- src/components/InputPanel.vue
+++ src/components/InputPanel.vue
@@ -1,26 +1,29 @@
 <script setup>
 import { ref } from 'vue'
 const time = 90
-const isNumDecided = ref(false)
+const isSelectedDone = ref(false)
 const inputNumbers = ref([-1, -1, -1, -1])
 const Numbers = ref(['0', '1', '2', '3', '4', '5', '6', '7', '8', '9'])
 const emit = defineEmits(['decideInputNumbers'])
 
 function changeInputNumbers(num) {
-  if (!this.isNumDecided) {
+  if (!this.isSelectedDone) {
     let index = inputNumbers.value.indexOf(-1)
     inputNumbers.value[index] = num
-    if (index === 3) isNumDecided.value = true
+    if (index === 3) isSelectedDone.value = true
   }
 }
 function deleteInputNumbers() {
-  isNumDecided.value = false
+  isSelectedDone.value = false
   let index = inputNumbers.value.indexOf(-1)
   if (index === -1) inputNumbers.value[3] = -1
   else inputNumbers.value[index - 1] = -1
 }
 function decideInputNumbers() {
   emit('decideInputNumbers', inputNumbers.value)
+}
+function isNumPicked(num) {
+  return inputNumbers.value.indexOf(num) !== -1
 }
 </script>
 
@@ -29,7 +32,7 @@ function decideInputNumbers() {
     <div class="time">{{ time }}</div>
     <div class="input-num">
       <p>"あなたの数字を決めてください"</p>
-      <ul>
+      <ul class="flex-container">
         <li v-for="num in inputNumbers" :key="num">
           <p v-if="num !== -1">{{ num }}</p>
           <p v-else>?</p>
@@ -37,16 +40,19 @@ function decideInputNumbers() {
       </ul>
     </div>
     <div class="select-num flex-box">
-      <h1
+      <button
         v-for="num in Numbers"
+        :disabled="isNumPicked(num)"
         :key="num"
         class="m-1"
         @click="changeInputNumbers(num)"
       >
         {{ num }}
-      </h1>
+      </button>
     </div>
-    <button v-show="isNumDecided" @click="decideInputNumbers">決定</button>
+    <button :disabled="!isSelectedDone" @click="decideInputNumbers">
+      決定
+    </button>
     <button @click="deleteInputNumbers">delete</button>
   </div>
 </template>
