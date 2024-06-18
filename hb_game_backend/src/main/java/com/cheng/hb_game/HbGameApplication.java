bb9ac0af4e3676f96df260734a949b77242dafda
Foxsunshine <kaihatsu.chien.jan@gmail.com>
2024-06-18 21:20:38 +0900

    backend start

---
+++ hb_game_backend/src/main/java/com/cheng/hb_game/HbGameApplication.java
@@ -0,0 +1,13 @@
+package com.cheng.hb_game;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HbGameApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HbGameApplication.class, args);
+	}
+
+}
---
+++ hb_game_backend/src/test/java/com/cheng/hb_game/HbGameApplicationTests.java
@@ -0,0 +1,13 @@
+package com.cheng.hb_game;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HbGameApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
