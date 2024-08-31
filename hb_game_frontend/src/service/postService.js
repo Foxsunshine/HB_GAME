a500272d9bdb1b7db9daf379ab30787bc30766a3
Foxsunshine <kaihatsu.chien.jan@gmail.com>
2024-08-31 13:44:00 +0900

    test push

--- hb_game_frontend/src/service/postService.js
+++ hb_game_frontend/src/service/postService.js
@@ -1,6 +1,12 @@
 import { serviceInstance } from './service.js'
 
 export const postService = {
+  // login(username, password) {
+  //   return serviceInstance.post('/api/login', {
+  //     username: username,
+  //     password: password
+  //   })
+  // },ß
   postHitResult(data) {
     return serviceInstance.post('/postHitResult', data)
   }
